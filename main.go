6c386511fbf0cacf88fdcc2e616cae46787247e1
rolcho <roland@cimem.hu>
2024-02-19 09:50:56 +0000

    post event

--- main.go
+++ main.go
@@ -4,16 +4,36 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/rolcho/go-rest-api/models"
 )
 
 func main() {
 	server := gin.Default()
 
 	server.GET("/health", getHealth)
+	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
 
 	server.Run(":8080")
 }
 
 func getHealth(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, gin.H{"health": "ok"})
+}
+
+func getEvents(ctx *gin.Context) {
+	events := models.GetAllEvents()
+	ctx.JSON(http.StatusOK, events)
+}
+
+func createEvent(ctx *gin.Context) {
+	var event models.Event
+
+	if err := ctx.ShouldBindJSON(&event); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse data"})
+		return
+	}
+
+	event.Save()
+	ctx.JSON(http.StatusCreated, gin.H{"message": "event created", "event": &event})
 }
---
+++ models/events.go
@@ -0,0 +1,22 @@
+package models
+
+import "time"
+
+type Event struct {
+	Id          int
+	Title       string `binding:"required"`
+	Description string `binding:"required"`
+	Location    string `binding:"required"`
+	DateTime    time.Time
+	UserId      int
+}
+
+var events = []Event{}
+
+func (e Event) Save() {
+	events = append(events, e)
+}
+
+func GetAllEvents() []Event {
+	return events
+}
